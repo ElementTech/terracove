e96f4b367c4c22db30f63ac439776a8f7c6dd332
Amitai Getzler <amitai.getzler@explorium.ai>
2023-04-09 17:34:43 +0300

    :test_tube: fix tests
    modified:   pkg/report/report_test.go

--- pkg/report/report_test.go
+++ pkg/report/report_test.go
@@ -6,6 +6,7 @@ import (
 	"io/ioutil"
 	"os"
 	"testing"
+	"time"
 
 	"github.com/GoogleCloudPlatform/testgrid/metadata/junit"
 	"github.com/jatalocks/terracove/internal/types"
@@ -87,4 +88,87 @@ func TestCreateJson(t *testing.T) {
 			assert.NoError(t, err)
 		}
 	}
+}
+
+func TestCreateJunitStruct(t *testing.T) {
+	// Create some sample data for testing
+	terraformStatuses := []types.TerraformModuleStatus{
+		{
+			Path: "module1",
+			Results: []types.Result{
+				{
+					Path:              "resource1",
+					Duration:          time.Duration(1) * time.Second,
+					ResourceCount:     2,
+					ResourceCountDiff: 1,
+					ActionDeleteCount: 0,
+					ActionUpdateCount: 0,
+					ActionReadCount:   0,
+					ActionCreateCount: 1,
+					ActionNoopCount:   0,
+					Coverage:          50,
+				},
+			},
+		},
+	}
+
+	// Call the function being tested
+	junitSuites, err := CreateJunitStruct(terraformStatuses)
+	if err != nil {
+		t.Errorf("CreateJunitStruct returned an error: %v", err)
+	}
+
+	// Verify the result
+	if len(junitSuites.Suites) != 1 {
+		t.Errorf("CreateJunitStruct returned %d suites, expected 1", len(junitSuites.Suites))
+	}
+	suite := junitSuites.Suites[0]
+	if suite.Name != "module1" {
+		t.Errorf("CreateJunitStruct returned a suite with name %s, expected 'module1'", suite.Name)
+	}
+	if suite.Tests != 1 {
+		t.Errorf("CreateJunitStruct returned a suite with %d tests, expected 1", suite.Tests)
+	}
+	if suite.Failures != 1 {
+		t.Errorf("CreateJunitStruct returned a suite with %d failures, expected 1", suite.Failures)
+	}
+	result := suite.Results[0]
+	if result.Name != "resource1" {
+		t.Errorf("CreateJunitStruct returned a result with name %s, expected 'resource1'", result.Name)
+	}
+	if result.Time != 1.0 {
+		t.Errorf("CreateJunitStruct returned a result with time %f, expected 1.0", result.Time)
+	}
+}
+
+func TestPrettyPrinter(t *testing.T) {
+	ts := junit.Suites{
+		Suites: []junit.Suite{
+			{
+				Results: []junit.Result{
+					{
+						Name: "Test case 1",
+						Failure: &junit.Failure{
+							Message: "Assertion failed",
+						},
+					},
+					{
+						Name:    "Test case 2",
+						Skipped: &junit.Skipped{},
+					},
+					{
+						Name: "Test case 3",
+						Errored: &junit.Errored{
+							Message: "Unexpected error",
+						},
+					},
+					{
+						Name: "Test case 4",
+					},
+				},
+			},
+		},
+	}
+
+	PrettyPrinter(ts)
 }
