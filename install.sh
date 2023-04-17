11b2a84cb05826d6d98d0415d56175b52f9df1a3
Amitai Getzler <amitai.getzler@explorium.ai>
2023-04-17 13:18:31 +0300

    :pencil2: fix
    modified:   action.yaml; modified:   install.sh; modified:   main.go

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-curl -L 'https://github.com/jatalocks/terracove/releases/download/v0.0.6/terracove_0.0.6_Darwin_x86_64.tar.gz' | tar -xz terracove
+curl -L 'https://github.com/jatalocks/terracove/releases/download/v0.0.7/terracove_0.0.7_Darwin_x86_64.tar.gz' | tar -xz terracove
 chmod u+x terracove
 mv terracove /usr/local/bin/terracove
 terracove --version
